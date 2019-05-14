2b0ea69f558eabfe6f6042935c673e35274bd2e3
bgabbasov <bgabbasov@gmail.com>
2019-05-14 22:53:56 +0300

    add tests

--- src/main/java/com/aurea/jervistest/Program.java
+++ src/main/java/com/aurea/jervistest/Program.java
@@ -2,6 +2,10 @@ package com.aurea.jervistest;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println(getGreeting("world"));
+    }
+
+    public static String getGreeting(String name) {
+        return "Hello, " + name + "!";
     }
 }
